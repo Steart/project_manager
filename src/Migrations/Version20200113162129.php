85afa262179b360e83907302915b5a4baf769b3b
ArthonVanDijk <arthon@duracom.nl>
2020-01-13 17:29:26 +0100

    Added doctrine migrations to write the entity changes to the database. For more information see the previous commits.

---
+++ src/Migrations/Version20200113162129.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200113162129 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project ADD contact_id INT NOT NULL');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EEE7A1254A FOREIGN KEY (contact_id) REFERENCES contact (id)');
+        $this->addSql('CREATE INDEX IDX_2FB3D0EEE7A1254A ON project (contact_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EEE7A1254A');
+        $this->addSql('DROP INDEX IDX_2FB3D0EEE7A1254A ON project');
+        $this->addSql('ALTER TABLE project DROP contact_id');
+    }
+}
