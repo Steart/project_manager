f21d032d2c72f8b76032f6933d6e596e8dca3d49
ArthonVanDijk <arthon@duracom.nl>
2020-01-17 08:29:45 +0100

    Fixed bug in contact entity causing the doctrine to ruin contacts page.

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ORM\Table(name="contacts")
  * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
  */
 class Contact
